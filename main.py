cee4d6efdd7ae06d0c61cfd98bb31c85027aec0e
AlejoGarcia1 <152428912+AlejoGarcia1@users.noreply.github.com>
2024-10-30 15:59:55 +0100

    comments main.py

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from UserManager import UserAuthenticator
 
+#Inicio de la funcion que se ejecutara al arrancar el programa.
 def main():
     print("Welcome!")
+
+    #Bucle que se ejecutara hasta que el usuario decida salir.
     while True:
         print("\nPlease choose an option:")
         print("1. Register")
         print("2. Login")
         print("3. Exit")
 
+        #Seleccion de la opcion.
         option = input("Select an option (1, 2, or 3): ")
         if option == "1":
+            #Registro de un nuevo usuario.
             success = UserAuthenticator.register()
             if success:
                 print("You may now log in with your new account.")
         elif option == "2":
+            # Si el usuario se ha logueado correctamente, se ejecutara el bucle de la sesion.
             session = UserAuthenticator.login()
             if session:
                 while True:
@@ -33,6 +39,7 @@ def main():
                     else:
                         print("Invalid option.")
         elif option == "3":
+            #Salida del programa.
             print("Exiting program.")
             break
         else:
