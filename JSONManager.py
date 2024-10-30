61fc7f8c6941cc5fdddd0122c1691f4e0a523cba
elnisha <1alvarezsanchez66@gmail.com>
2024-10-30 16:02:29 +0100

    Comentarios JSONManager y readme

--- JSONManager.py
+++ JSONManager.py
@@ -3,21 +3,25 @@ import os
 
 
 class JSONManager:
+    """ Clase para la gestión de archivos JSON. """
     USER_DATA_FILE = "users.json"
     PUBLIC_KEY_FILE = "public_keys.json"
+
+    # Método para cargar los datos de los usuarios desde users.json
     @staticmethod
     def load_user_data():
         if os.path.exists(JSONManager.USER_DATA_FILE):
             with open(JSONManager.USER_DATA_FILE, "r") as f:
                 return json.load(f)
         return {}
-
+    
+    # Método para guardar los datos de los usuarios en users.json
     @staticmethod
     def save_user_data(user_data):
         with open(JSONManager.USER_DATA_FILE, "w") as f:
             json.dump(user_data, f, indent=4)
 
-    # New method to save a user's public key in public_keys.json
+    # Método para guardar la clave pública de un usuario en public_keys.json
     @staticmethod
     def save_public_key(username, public_key_pem):
         if os.path.exists(JSONManager.PUBLIC_KEY_FILE):
@@ -30,7 +34,7 @@ class JSONManager:
         with open(JSONManager.PUBLIC_KEY_FILE, "w") as f:
             json.dump(public_keys, f, indent=4)
 
-    # New method to load a user's public key from public_keys.json
+    # Método para cargar la clave pública de un usuario desde public_keys.json
     @staticmethod
     def load_public_key(username):
         if os.path.exists(JSONManager.PUBLIC_KEY_FILE):
@@ -38,3 +42,4 @@ class JSONManager:
                 public_keys = json.load(f)
                 return public_keys.get(username)
         return None
+
--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-# Eval1Cripto
+# ProyectoCripto2024
 
-Esto es un nuevo repositorio creado con el mismo codigo pero "refactored" ya que el repositorio antiguo no estaba para nada bien organizado.
+## Members
+- Juan Álvarez Sánchez (NIA: 100495738)
+- Luis Valero Montero (NIA: 100495982)
+- Alejo García Maldonado (NIA: )
+
+## Links of Interest
+- **Carpeta de Google Drive:** https://drive.google.com/drive/folders/1iRO5GhIbhRZj0c30cE0yqjEGp-oapfW9?usp=drive_link
+- **Enunciado de la Práctica:** https://aulaglobal.uc3m.es/pluginfile.php/7318656/mod_resource/content/1/Enunciado_practica_24_25.pdf 
+    - (También esta en ProyectoCripto2024/Documentation)
+
+## Evaluación 
+El proyecto se divide en 2 entregas. 
+
+### Evaluación Entrega 1:
+- *Autenticación de usuarios* ($0,75$)
+- *Cifrado simétrico/asimétrico* ($0,75$)
+- *Etiquetas de autenticación de mensajes* ($0,75$)
+- Calidad del software ($0,25$)
+- Memoria ($1,5$)
+- Defensa ($1$)
+
+**Puntuación Total Entrega 1**: $0.75 + 0.75 + 0.75 + 0.25 + 1.5 + 1 = \boxed{5}$
+
